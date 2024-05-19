2b8699b6096f27b51a022383b8e67dd5c17f4a6f
aayushsingh9596 <aayushsingh9596@gmail.com>
2024-05-19 22:01:07 +0530

    t commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div className="bg-blue-800 py-10">
+    <div className="bg-blue-800 py-10 px-1">
         <div className="container mx-auto flex justify-between items-center">
             <span className="text-3xl text-white font-bold tracking-tight">
                 BookingApp
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   const { isLoggedIn } = useAppContext();
 
   return (
-    <div className="bg-blue-800 py-6">
+    <div className="bg-blue-800 py-6 px-1">
       <div className="container mx-auto flex justify-between">
         <span className="text-3xl text-white font-bold tracking-tight">
           <Link to="/">BookingApp</Link>
